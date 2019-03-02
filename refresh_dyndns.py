f493a75fc9221c1b61e645fc1227e1403c34dece
Panzki <battleknight95@web.de>
2019-03-02 12:09:05 +0100

    Implemented command line arguments.

--- refresh_dyndns.py
+++ refresh_dyndns.py
@@ -1,12 +1,22 @@
-import urllib.request
-from secrets import userId, apiKey, domain
+import urllib.request, argparse
 
-refreshUrl = f'https://api.org-dns.com/dyndns/?user={userId}&key={apiKey}&domain={domain}'
-print(f'Attempting to refresh dns record by fetching: {refreshUrl}')
-response = str(urllib.request.urlopen(refreshUrl).read())
-if 'not updated' in response:
-    print('[SUCCESS] Dns record already up to date.')
-elif 'success' in response and 'record updated' in response:
-    print('[SUCCESS] Dns record was successfully updated.')
-else:
-    print(f'[FAIL] Dns record could not be updated, response is: {response}')
+def parseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--userId", type=str, required=True, help="Your wint.global user id.")
+    parser.add_argument("-k", "--apiKey", type=str, required=True, help="Your wint.global api password.")
+    parser.add_argument("-d", "--domain", type=str, required=True, help="Your wint.global domain that will be updated.")
+    arguments = parser.parse_args()
+
+    return (arguments.userId, arguments.apiKey, arguments.domain)
+
+if __name__ == "__main__":
+    userId, apiKey, domain = parseArguments()
+    refreshUrl = f'https://api.org-dns.com/dyndns/?user={userId}&key={apiKey}&domain={domain}'
+    print(f'Attempting to refresh dns record by fetching: {refreshUrl}')
+    response = str(urllib.request.urlopen(refreshUrl).read())
+    if 'not updated' in response:
+        print('[SUCCESS] Dns record already up to date.')
+    elif 'success' in response and 'record updated' in response:
+        print('[SUCCESS] Dns record was successfully updated.')
+    else:
+        print(f'[FAIL] Dns record could not be updated, response is: {response}')
